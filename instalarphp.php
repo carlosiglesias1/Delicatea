d7811bb95a88e28e87a9178dce47b8931b6ab42e
carliños <carlosig490@gmail.com>
2021-06-24 10:19:54 +0200

    funciona

--- instalarphp.php
+++ instalarphp.php
@@ -1,12 +1,13 @@
 <?php
-$config = include 'Conexion/config.php';
 
+$config = include 'Conexion/config.php';
+include 'Lenguajes/es.php';
 try {
     $conexion = new PDO('mysql:host=' . $config['db']['host'], $config['db']['user'], $config['db']['pass'], $config['db']['options']);
     $sql = file_get_contents("data/migracion.sql");
 
     $conexion->exec($sql);
-    echo <?php echo $lang['Inicio'] ?>;
+    echo  $lang['Inicio'];
 } catch (PDOException $error) {
     echo $error->getMessage();
 }
